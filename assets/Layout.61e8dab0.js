b6cb8e309e53b9a2b6e7cef461e1e4ec6e91c757
yahu39 <yahu39pc@gmail.com>
2022-11-05 19:00:01 -0500

    adding dist subtree

---
+++ assets/Layout.61e8dab0.js
@@ -0,0 +1,1 @@
+import{h as r,p as s,k as a,r as e,q as m,s as n}from"./app.5794bb3e.js";import{S as p}from"./SkipLink.ced1e5b7.js";var l=r({name:"Layout",setup(){const o=n(),t=s();return()=>[a(p),a(e("CommonWrapper"),()=>t.value.home?a(e("HomePage")):a(m,()=>a(e("NormalPage"),{key:o.value.path})))]}});export{l as default};
