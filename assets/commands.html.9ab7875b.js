e071902bc3a346ae5831c641c60477ff3ed710ee
yahu39 <yahu39pc@gmail.com>
2022-11-09 02:55:48 -0500

    export import from csv mysql

---
+++ assets/commands.html.9ab7875b.js
@@ -0,0 +1,1 @@
+import{_ as e,o as a,a as o,b as s,d as t}from"./app.cdb84592.js";const c={},n={id:"commands-vim",tabindex:"-1"},d=s("a",{class:"header-anchor",href:"#commands-vim","aria-hidden":"true"},"#",-1),r=t(" Commands VIM"),_=[d,r];function m(i,h){return a(),o("h1",n,_)}var l=e(c,[["render",m]]);export{l as default};
---
+++ assets/nvim.html.d2537665.js
@@ -0,0 +1,1 @@
+import{_ as a,o as n,a as t,F as r,b as e,d as s}from"./app.cdb84592.js";const c={},o=e("h1",{id:"nvim",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#nvim","aria-hidden":"true"},"#"),s(" NVim")],-1),d=e("p",null,"Instalacion de NVIM",-1);function i(_,l){return n(),t(r,null,[o,d],64)}var h=a(c,[["render",i]]);export{h as default};
